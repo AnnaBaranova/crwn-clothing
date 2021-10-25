36514749e8312b4a3b69e5555f55bb452611d1cb
Anna Baranova <asbaranova1@gmail.com>
2021-10-25 14:24:22 -0400

    add shop page, collection componenent

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import './shop.data.js'
+import SHOP_DATA from "./shop.data";
+
+import PreviewCollection from "../../components/preview-collection/preview-collection.component.jsx";
+
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: SHOP_DATA
+    };
+  }
+
+  render() {
+      const {collections} = this.state
+      return (
+      <div>
+          {
+              collections.map(({id, ...otherCollectionProps} )=> (
+                  <PreviewCollection key={id} {...otherCollectionProps} />
+              ))}
+      </div>)
+  }
+}
+
+export default ShopPage
